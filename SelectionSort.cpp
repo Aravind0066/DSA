81d6113b8b5c8984db566d8058b9c83d3adc60cb
Aravind0066 <aravind.m0006@gmail.com>
2025-08-03 20:22:26 +0530

    Sorting 1

---
+++ SelectionSort.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+void selection_sort(int arr[], int n) {
+  // selection sort                             /// selection sort is all about finding minimum and swapping with the elements starting from the start
+  for (int i = 0; i < n - 1; i++) {
+    int mini = i;
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[mini]) {
+        mini = j;                           /// we find the minimum element and swap with the i element thus we use 2 loops;
+      }
+    }
+    int temp = arr[mini];          /// The swapping part // can also use a function for this..
+    arr[mini] = arr[i];
+    arr[i] = temp;
+  }
+
+  cout << "After selection sort: " << "\n";
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << "\n";
+
+}
+int main() {
+  int arr[] = {13,46,24,52,20,9};
+  int n = sizeof(arr) / sizeof(arr[0]);
+   cout << "Before selection sort: " << "\n";
+   for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << "\n";
+  selection_sort(arr, n);
+  return 0;
+}
