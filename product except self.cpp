095dd3b9e3709396968b88eb171ba5dcb61d839d
Aravind0066 <aravind.m10@outlook.com>
2026-01-31 12:51:45 +0530

    new

---
+++ product except self.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int output[n];
+    output[0] = 1;
+    for(int i=1; i<n; i++){
+        output[i] = output[i-1]*arr[i-1];
+    }
+
+    int right = 1;
+    for(int i=n-1; i>=0; i--){
+        output[i] *= right;
+        right *= arr[i];
+    }
+
+    for(int i=0; i<n; i++){
+        cout << output[i];
+    }
+
+    return 0;
+}
